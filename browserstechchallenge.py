72deeac61766d944fe5d8f970ca3c03a11a89214
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-02 18:32:14 -0400

    Update browserstechchallenge.py

--- browserstechchallenge.py
+++ browserstechchallenge.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
+import os
 
 # BrowserStack Credentials
 username = os.environ.get('BROWSERSTACK_USERNAME')
