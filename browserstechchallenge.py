8ed77ae5199071a55e041d02829682963048df6e
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-03 00:54:34 -0400

    Update browserstechchallenge.py

--- browserstechchallenge.py
+++ browserstechchallenge.py
@@ -1,4 +1,3 @@
-from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options as ChromeOptions
