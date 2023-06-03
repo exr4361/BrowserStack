d6cb8ea229b441da0181a4a6c06197c1664947d2
exr4361 <47603726+exr4361@users.noreply.github.com>
2023-06-02 21:52:55 -0400

    Update browserstechchallenge.py

--- browserstechchallenge.py
+++ browserstechchallenge.py
@@ -14,10 +14,10 @@ import json
 import os
 
 # Environment Variables
-BROWSERSTACK_USERNAME = os.environ.get('BROWSERSTACK_USERNAME')
-BROWSERSTACK_ACCESS_KEY = os.environ.get('BROWSERSTACK_ACCESS_KEY')
+BROWSERSTACK_USERNAME = os.environ.get("BROWSERSTACK_USERNAME")
+BROWSERSTACK_ACCESS_KEY = os.environ.get("BROWSERSTACK_ACCESS_KEY")
 URL = "https://hub.browserstack.com/wd/hub" 
-buildName = os.environ.get('JENKINS_LABEL')
+buildName = os.environ.get("JENKINS_LABEL")
 
 # BrowserStack Trial credentials
 bs_email = os.getenv('BS_USR')  # BrowserStack email from Jenkinsfile
